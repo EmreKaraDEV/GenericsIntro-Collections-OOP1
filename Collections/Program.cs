09be255989886ab77a395854f144b22a62f9c65b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-02 14:27:46 +0300

    Proje dosyası ekle.

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+            names.Add("İlker");
+            Console.WriteLine(names[4]);
+            Console.WriteLine(names[0]);
+        }
+    }
+}
